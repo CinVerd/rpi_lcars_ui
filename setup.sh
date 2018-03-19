8977c5d8562d94725bb604e425bf6d9ef5f8ad6f
cinve <delta36@gmail.com>
2018-03-19 20:03:58 +0000

    Updating setup script to include python-vlc.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo apt-get update
-sudo apt-get install python-pygame python-pil
+sudo apt-get install python-pygame python-pil python-vlc
 
 # on dietpi/raspbian lite install xinit
 sudo apt-get install xinit
